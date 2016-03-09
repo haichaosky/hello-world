ecfb88eb49c0e36ab952c9c0ba419fca2993ca1e
13326136@qq.com <wanghaichao@wanghaichao.Hisense.ad>
2016-03-09 12:03:40 +0800

    add hell.c

---
+++ hellow.c
@@ -0,0 +1,22 @@
+#include <linux/init.h>
+#include <linux/module.h>
+
+static int hello_init(void)
+{
+	printk(KERN_INFO, "hello world enter.\n");
+	return 0;
+}
+
+static int hello_exit(void)
+{
+	printk(KERN_INFO, "hello world exit.\n");
+	return 0;
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
+
+MODULE_AUTHOR("wanghaichao <wanghaichao@hisense.com>");
+MODULE_LISENSE("Dual BSD/GPL");
+MODULE_DESCRIPTION("a hello world");
+MODULE_ALIAS("a simple module");
